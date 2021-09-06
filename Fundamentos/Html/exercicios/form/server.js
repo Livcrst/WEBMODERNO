51866af8d9b8c27530238d65212b4bf9256bc316
lívia <livcristinna@gmail.com>
2021-09-06 16:49:48 -0300

    forms

---
+++ Fundamentos/Html/exercicios/form/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json({ type: 'application/json' }));
+
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded());
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body);
+    resp.status(200).send('Deu bom hein');
+});
+
+app.listen(3003);
+
