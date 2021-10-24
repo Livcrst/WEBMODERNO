30a49df0f1af2efcc630bd7697528dcb2a7a7282
lívia <livcristinna@gmail.com>
2021-10-24 17:10:41 -0300

    destructuring

---
+++ Fundamentos/JavaScript/Conteudo/desestruturing.js
@@ -0,0 +1,10 @@
+const rand = ({min = 0, max = 100}) => {
+
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+
+}
+
+const obj = { max: 50, min: 40};
+console.log(rand(obj));
+console.log(rand({min:995}));
