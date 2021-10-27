4bce0c118652fab26bd55fb18795f08b7a40a6c7
lívia <livcristinna@gmail.com>
2021-10-27 11:34:21 -0300

    Factory Function

--- Fundamentos/JavaScript/functions/declarationType.js
+++ Fundamentos/JavaScript/functions/declarationType.js
@@ -14,3 +14,18 @@ const sub = function (x,y){
 const mult = function mult (x,y){
     return x*y;
 }
+
+
+// Closures é o escopo criado quando uma função é declarada;
+// Esse escopo permite a função acessar e manipular variáveis externas á função.
+
+// Função Factory é uma função que retorna um objeto
+
+const factory = (name, age ) =>{
+    return {
+        nome: name,
+        idade: age
+    }
+}
+
+console.log( factory('Lívia', 22));
