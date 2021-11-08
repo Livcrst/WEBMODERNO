74081edb8d013c31058b01222056ec961c8b14c4
lívia <livcristinna@gmail.com>
2021-11-08 10:05:51 -0300

    object

--- Fundamentos/JavaScript/POO/Literal-notation.js
+++ Fundamentos/JavaScript/POO/Literal-notation.js
@@ -4,9 +4,10 @@ const ob1 = {}
 
 // Objeto no JavaScript
 
-const obj2 = new Object;
+const obj2 = new Object; // Funçao construtora e o operador new.
 
-// Funções contrutoras para
+
+// Função Factory :: Função que retorna um objeto.
 function Produto (nome, preco, desc) {
     this.nome = nome;
     // uma função ela pode ser publica ou privada, sendo possivel acessa-la fora do escopo da função, fazendo uso do this.
@@ -16,3 +17,23 @@ function Produto (nome, preco, desc) {
 
     }
 }
+
+
+const funcionario = (nome, salario, faltas) => {
+
+    return {
+        nome,
+        salario, 
+        faltas,
+        getSalario (){
+            return (salario/30)*(30-faltas);
+        }
+    }
+
+}
+
+// É possivel criar um objeto por meio de Object.create ()
+
+
+const filha = Object.create(null);
+
