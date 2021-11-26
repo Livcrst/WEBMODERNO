63809c8ad7255d62e613c26ecb623d3b4d7ac1fb
lívia <livcristinna@gmail.com>
2021-11-25 22:23:13 -0300

    teste

--- Fundamentos/JavaScript/Array/arrayJS.js
+++ Fundamentos/JavaScript/Array/arrayJS.js
@@ -1,2 +1,5 @@
 // Um array no JavaScript é um objeto, não um tipo de dado.
 // Javascript não obriga o array a ter somente dados de um unico tipo, mas o recomendado é que vcv não mistura.
+
+
+dfskhlnjadsgfjklhbnsfdiukljhn
